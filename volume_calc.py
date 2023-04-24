e40e96b31754e56689a846403ec0754a0c85a964
meeeeeeil <130380932+meeeeeeil@users.noreply.github.com>
2023-04-24 20:10:37 +0900

    Update volume_calc.py
    총 길이에 따른 요금계산 기능 추가

--- volume_calc.py
+++ volume_calc.py
@@ -2,6 +2,22 @@ width = float(input("width: "))
 depth = float(input("depth: 0"))
 height = float(input("height: "))
 
-volume = width * depth * height
+volume = width + depth + height
 
-print("Volume is", volume)
+print("Volume:", volume)
+
+total_length = depth + width + height
+
+if total_length <= 80:
+    charge = 5000
+elif total_length <=100:
+    charge = 8000
+elif total_length <=120:
+    charge = 10000
+elif total_length <=160:
+    charge = 13000
+else:
+    charge = "unvaliable"
+
+print("Total length:", total_length)
+print("Charge:", charge)
